42fa81c1815133039747deac722372ef2b92e7a5
Sebastian <shuertas981@gmail.com>
2024-05-29 22:50:49 -0500

    Agregando tabla categorias y marcas en la bd

--- sistema_cotizacion.sql
+++ sistema_cotizacion.sql
@@ -8,6 +8,16 @@ id_rol int auto_increment primary key,
 nom_rol varchar (150) 
 );
 
+create table marcas
+(
+id_marca int auto_increment primary key,
+nom_marca varchar (150) 
+);
+create table categorias
+(
+id_cat int auto_increment primary key,
+nom_cat varchar (150) 
+);
 insert into roles values (1,"Administrador");
 insert into roles values (2,"Supervisor");
 insert into roles values (3,"Vendedor");
@@ -16,11 +26,15 @@ create table usuarios
 (
 id int auto_increment primary key,
 usuario varchar (150),
-password varchar (50),
+password varchar (255),
 nombre varchar (150),
 rol int,
 constraint fk_roles foreign key (rol) references roles(id_rol)
 );
-ALTER TABLE usuarios MODIFY password VARCHAR(255);
 
-select * from usuarios
+select * from usuarios;
+select * from roles;
+
+SELECT * FROM roles WHERE id_rol = 1;
+DESCRIBE roles;
+
